0718bc3666c5acd5476cd6fd8236d26e5ad8b594
iqbal nurrizqi <iqbalnurrizqi2@gmail.com>
2023-10-21 05:33:41 +0700

    update revisi pertama

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,13 +3,18 @@ import Note from "./Note";
 import NoteArchive from "./NoteArchive";
 
 export default function Body({notes, onRemoveNote, onMoveNote, search }) {
+    const noteActive = notes.filter((note) => !note.archived);
+    const noteArchive = notes.filter((note) => note.archived);
+
+
     return (
       <div className="note-app__body">
         <div className="note">
           <h1>Catatan Aktif</h1>
         </div>
         <div className="notes-list">
-          {notes.filter((note) => !note.archived || search === note.title ? note.title.toLowerCase().includes(search.toLowerCase()) : null).map((note) => (
+          {noteActive.length === 0 ? <p className="notes-list__empty-message">Tidak ada catatan</p> :
+          notes.filter((note) => !note.archived || search === note.title ? note.title.toLowerCase().includes(search.toLowerCase()) : null).map((note) => (
              <Note note={note} key={note.id} id={note.id} {...note} onRemoveNote={onRemoveNote} onMoveNote={onMoveNote}/>          
            ))} 
            
@@ -18,7 +23,8 @@ export default function Body({notes, onRemoveNote, onMoveNote, search }) {
               <h1>Arsip</h1>
             </div>
               <div className="notes-list">
-                  {notes.filter((note) => note.archived || search === note.title ? note.title.toLowerCase().includes(search.toLowerCase()) : null ).map((note) => (                   
+                  {noteArchive.length === 0 ? <p className="notes-list__empty-message">tidak ada catatan </p> :
+                  notes.filter((note) => note.archived || search === note.title ? note.title.toLowerCase().includes(search.toLowerCase()) : null ).map((note) => (                   
                   <NoteArchive note={note} key={note.id} id={note.id} {...note} onRemoveNote={onRemoveNote} onMoveNote={onMoveNote}/>                    
                   ))}
               </div>
